ad8b8f38a16ac6db711e5b6ad6fd5d788616a046
niklasvoss <golden.horse0912@gmail.com>
2024-12-07 03:49:48 +0200

    Mathew

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Yaroslav Temchenko</strong>
+                <strong className="main-name"> Mathew </strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
